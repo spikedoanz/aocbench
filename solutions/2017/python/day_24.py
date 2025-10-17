d47ee8b1910e41bc0618aceb5675dad85ad797b1
spike <spikedoanz@gmail.com>
2025-10-17 16:49:35 -0400

    fixed solution for day 24

--- solutions/2017/python/day_24.py
+++ solutions/2017/python/day_24.py
@@ -4,19 +4,24 @@ INPUT_DIR = os.path.expanduser(os.getenv('AOC_INPUT_DIR', '~/.cache/aocb/inputs/
 d = [tuple(int(y) for y in x.strip().split('/')) for x in open(os.path.join(INPUT_DIR, "2017_24.txt")).readlines()]
 
 
-def build(start, rem, maxlen=0, score=0):
-    best = []
+def build(start, rem, part2=False):
+    best = (0, 0)  # (length, strength)
     for left, right in rem:
         if start in (left, right):
             subrem = rem[:]
             subrem.remove((left, right))
-            myscore = score + left + right
             x = right if left == start else left
-            cand = [(myscore, left, right)] + build(x, subrem, maxlen, myscore)
-            best = cand if not best or (maxlen and len(cand) > len(best)) \
-                or cand[-1] > best[-1] else best
+            sub_len, sub_str = build(x, subrem, part2)
+            total_len = sub_len + 1
+            total_str = sub_str + left + right
+            if part2:
+                if total_len > best[0] or (total_len == best[0] and total_str > best[1]):
+                    best = (total_len, total_str)
+            else:
+                if total_str > best[1]:
+                    best = (total_len, total_str)
     return best
 
 
-print(build(0, d)[-1][0])
-print(build(0, d, 1)[-1][0])
+print(build(0, d, False)[1])
+print(build(0, d, True)[1])
